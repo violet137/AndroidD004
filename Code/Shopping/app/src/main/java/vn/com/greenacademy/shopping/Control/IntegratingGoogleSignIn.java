a8a4346b3f4d9468ca1dcac38302f76f0fa806cb
DESKTOP-BURRAHA\ADMIN <duongquocnghi2016@gmail.com>
2017-07-04 21:09:06 +0700

    code

---
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Control/IntegratingGoogleSignIn.java
@@ -0,0 +1,8 @@
+package vn.com.greenacademy.shopping.Control;
+
+/**
+ * Created by ADMIN on 7/4/2017.
+ */
+
+public class IntegratingGoogleSignIn {
+}
--- Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/MainActivity.java
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/MainActivity.java
@@ -1,14 +1,75 @@
 package vn.com.greenacademy.shopping;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    SignInButton signInButton;
+    GoogleApiClient mGoogleApiClient;
+    int RC_SIGN_IN = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        getSupportActionBar().hide();
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+
+        signInButton.setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in_button:
+                googleSignIn();
+                break;
+
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Toast.makeText(this, result.getSignInAccount().getPhotoUrl().toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, 200);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
