7b5090176308862cc1bed326826e8b53f0dfb2a4
DESKTOP-BURRAHA\ADMIN <duongquocnghi2016@gmail.com>
2017-07-18 20:41:06 +0700

    tạo asyn talk get main menu photo

---
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Interface/UrlPhotoCallBack.java
@@ -0,0 +1,11 @@
+package vn.com.greenacademy.shopping.Interface;
+
+import vn.com.greenacademy.shopping.Model.MenuPhoto;
+
+/**
+ * Created by ADMIN on 7/18/2017.
+ */
+
+public interface UrlPhotoCallBack {
+    void urlCallBack(MenuPhoto menuPhoto);
+}
