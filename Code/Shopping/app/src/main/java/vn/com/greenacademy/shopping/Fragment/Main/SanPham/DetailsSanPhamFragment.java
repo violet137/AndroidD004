43a49a5921a0cca45d07bc5ee7d3e84970e31c0e
MTTA <mtta200396@gmail.com>
2017-07-25 14:25:24 +0700

    Dialog chi tiết sản phẩm

--- Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/Main/SanPham/DetailsSanPhamFragment.java
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/Main/SanPham/DetailsSanPhamFragment.java
@@ -10,29 +10,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import vn.com.greenacademy.shopping.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailsSanPhamFragment extends Dialog {
+public class DetailsSanPhamFragment extends Dialog implements View.OnClickListener {
+    private String description;
+    private String details;
 
-
-    public DetailsSanPhamFragment(Context context) {
+    public DetailsSanPhamFragment(Context context, String description, String details) {
         super(context);
-        // Required empty public constructor
+        this.description = description;
+        this.details = details;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View root = getLayoutInflater().inflate(R.layout.dialog_details_san_pham, null);
+        TextView tvDescription = (TextView) root.findViewById(R.id.description_text_chi_tiet_san_pham);
+        TextView tvChiTiet = (TextView) root.findViewById(R.id.details_text_chi_tiet_san_pham);
 
-        setContentView(root);
-        this.getWindow().setBackgroundDrawableResource(R.drawable.scrim);
-        this.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        root.findViewById(R.id.thoat_button_chi_tiet_set_do).setOnClickListener(this);
+
+//        tvDescription.setText(description);
+//        tvChiTiet.setText(details);
+        configDialog(root);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.thoat_button_chi_tiet_set_do)
+            dismiss();
+    }
+
+    private void configDialog(View root) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(root);
+        this.getWindow().setBackgroundDrawableResource(R.drawable.scrim);
+        this.getWindow().setLayout((int) (this.getContext().getResources().getDimension(R.dimen.dialog_chi_tiet_san_pham_width) / this.getContext().getResources().getDisplayMetrics().density), (int) (getContext().getResources().getDimension(R.dimen.dialog_chi_tiet_san_pham_height) / this.getContext().getResources().getDisplayMetrics().density));
+    }
 }
