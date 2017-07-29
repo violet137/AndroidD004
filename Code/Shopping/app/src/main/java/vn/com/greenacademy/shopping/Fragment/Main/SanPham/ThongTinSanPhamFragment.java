3189b0e6710fbd3308d1cc698ac6045988bc6fd7
MTTA96 <mtta200396@gmail.com>
2017-07-29 17:16:54 +0700

    no message

--- Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/Main/SanPham/ThongTinSanPhamFragment.java
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/Main/SanPham/ThongTinSanPhamFragment.java
@@ -9,15 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import vn.com.greenacademy.shopping.Handle.HandleUi.Adapter.SanPham.SanPhamPagerAdapter;
+import vn.com.greenacademy.shopping.Handle.HandleUi.Model.QuickActionItem;
+import vn.com.greenacademy.shopping.Handle.HandleUi.SanPham.QuickActionPopup;
 import vn.com.greenacademy.shopping.Model.SanPham;
 import vn.com.greenacademy.shopping.R;
 import vn.com.greenacademy.shopping.Util.Ui.BaseFragment;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
 import static vn.com.greenacademy.shopping.Util.SupportKeyList.TAG_DETAILS_SAN_PHAM;
 
 /**
@@ -25,6 +30,7 @@ import static vn.com.greenacademy.shopping.Util.SupportKeyList.TAG_DETAILS_SAN_P
  */
 public class ThongTinSanPhamFragment extends Fragment implements View.OnClickListener {
     Button btnSizeInfo;
+    private QuickActionPopup quickActionPopup;
 
     private BaseFragment baseFragment;
     private ArrayList<SanPham> listSanPham = new ArrayList<>();
@@ -43,9 +49,14 @@ public class ThongTinSanPhamFragment extends Fragment implements View.OnClickLis
         final TextView tvTenVaMau = (TextView) root.findViewById(R.id.ten_va_mau_fragment_san_pham);
         final TextView tvSoLuong = (TextView) root.findViewById(R.id.so_luong_fragment_san_pham);
         ViewPager pagerSanPham = (ViewPager) root.findViewById(R.id.pager_fragment_san_pham);
-        btnSizeInfo = (Button) root.findViewById(R.id.button_size_info_san_pham);
+        btnSizeInfo = (Button) root.findViewById(R.id.button_size_san_pham);
 
         root.findViewById(R.id.button_info_san_pham).setOnClickListener(this);
+        root.findViewById(R.id.button_san_pham_khac).setOnClickListener(this);
+        root.findViewById(R.id.button_hinh_san_pham).setOnClickListener(this);
+        root.findViewById(R.id.button_color_san_pham).setOnClickListener(this);
+        root.findViewById(R.id.button_size_san_pham).setOnClickListener(this);
+        root.findViewById(R.id.button_them_san_pham).setOnClickListener(this);
         btnSizeInfo.setOnClickListener(this);
 
         baseFragment = new BaseFragment(getActivity().getSupportFragmentManager());
@@ -79,11 +90,58 @@ public class ThongTinSanPhamFragment extends Fragment implements View.OnClickLis
             case R.id.button_info_san_pham:
                 new DetailsSanPhamFragment(getActivity(),null,null).show();
                 break;
-            case R.id.button_size_info_san_pham:
-                PopupMenu popupMenu = new PopupMenu(getActivity(), btnSizeInfo);
-                popupMenu.inflate(R.menu.menu_tool_bar);
-                popupMenu.show();
+            case R.id.button_san_pham_khac:
+                Toast.makeText(getActivity(), "Sản phẩm khác", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button_hinh_san_pham:
+                setUpQuickAction();
+                quickActionPopup.show(v);
+                break;
+            case R.id.button_color_san_pham:
+                setUpQuickAction();
+                quickActionPopup.show(v);
+                break;
+            case R.id.button_size_san_pham:
+                setUpQuickAction();
+                quickActionPopup.show(v);
+                break;
+            case R.id.button_them_san_pham:
+                Toast.makeText(getActivity(), "Thêm vào giỏ hàng", Toast.LENGTH_LONG).show();
                 break;
         }
+    }
+
+    public void setUpQuickAction(){
+        final int ID_VLC    = 1;
+        final int ID_MAIL   = 2;
+        final int ID_SAFARI = 3;
+
+        QuickActionItem mailItem    = new QuickActionItem(ID_MAIL, null, getResources().getDrawable(R.drawable.red));
+        QuickActionItem vlcItem     = new QuickActionItem(ID_VLC, null, getResources().getDrawable(R.drawable.black));
+        QuickActionItem safariItem  = new QuickActionItem(ID_SAFARI, null, getResources().getDrawable(R.drawable.yellow));
+        //create QuickActionPopup. Use QuickActionPopup.VERTICAL or QuickActionPopup.HORIZONTAL //param to define orientation
+        quickActionPopup = new QuickActionPopup(getActivity(), QuickActionPopup.HORIZONTAL);
+
+        //add action items into QuickActionPopup
+        quickActionPopup.addActionItem(mailItem);
+        quickActionPopup.addActionItem(vlcItem);
+        quickActionPopup.addActionItem(safariItem);
+
+        //Set listener for action item clicked
+        quickActionPopup.setOnActionItemClickListener(new QuickActionPopup.OnActionItemClickListener() {
+            @Override
+            public void onItemClick(QuickActionPopup source, int pos, int actionId) {
+
+                //filtering items by id
+                if (actionId == ID_MAIL) {
+                    Toast.makeText(getApplicationContext(), "Mail clicked", Toast.LENGTH_SHORT).show();
+                } else if (actionId == ID_VLC) {
+                    Toast.makeText(getApplicationContext(), "VLC clicked", Toast.LENGTH_SHORT).show();
+                } else if(actionId == ID_SAFARI){
+                    Toast.makeText(getApplicationContext(), "Safari clicked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
     }
 }
