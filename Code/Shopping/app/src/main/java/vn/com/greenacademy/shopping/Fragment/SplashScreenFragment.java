8ece3d605873b45eadde2aac8a360d2c60cf05ae
MTTA96 <mtta200396@gmail.com>
2017-08-01 16:00:24 +0700

    Test loading screen

--- Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/SplashScreenFragment.java
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/SplashScreenFragment.java
@@ -44,40 +44,43 @@ public class SplashScreenFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_splash_screen, container, false);
 
         baseFragment = new BaseFragment(getActivity().getSupportFragmentManager());
+
         //Xin quyền
         if(Build.VERSION.SDK_INT >= 23) {
-            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // Should we show an explanation?
-                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    //Hiện giải thích về quyền và xin quyền
-                } else {
-                    // No explanation needed, we can request the permission.
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_CODE);
-                }
+            if (CheckPermission())
+                TiepTuc();
+        }
+        else TiepTuc();
+
+        return root;
+    }
+
+    private void TiepTuc() {
+        //Load màn hình splash
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                actionBar.show();
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                baseFragment.ChuyenFragment(new MainFragment(), SupportKeyList.TAG_FRAGMENT_MAIN, false);
+            }
+        }, DELAY_TIME);
+    }
+
+    private boolean CheckPermission() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                //Hiện giải thích về quyền và xin quyền
             } else {
-                //Load màn hình splash
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        actionBar.show();
-                        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-                        baseFragment.ChuyenFragment(new MainFragment(), SupportKeyList.TAG_FRAGMENT_MAIN, false);
-                    }
-                }, DELAY_TIME);
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_CODE);
             }
         } else {
-            //Load màn hình splash
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    actionBar.show();
-                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-                    baseFragment.ChuyenFragment(new MainFragment(), SupportKeyList.TAG_FRAGMENT_MAIN, false);
-                }
-            }, DELAY_TIME);
+            return true;
         }
-        return root;
+        return false;
     }
 
 }
