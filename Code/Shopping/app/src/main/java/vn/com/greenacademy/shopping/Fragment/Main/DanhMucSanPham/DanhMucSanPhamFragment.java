ef51f5f8fe80aac345e20599bee67e9f489a53c0
Dương Quốc Nghi <duongquocnghi2016@gmail.com>
2017-08-13 16:07:33 +0700

    danh muc san pham+

--- Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/Main/DanhMucSanPham/DanhMucSanPhamFragment.java
+++ Code/Shopping/app/src/main/java/vn/com/greenacademy/shopping/Fragment/Main/DanhMucSanPham/DanhMucSanPhamFragment.java
@@ -24,7 +24,8 @@ public class DanhMucSanPhamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_danh_muc_san_pham, container, false);
+        View view = inflater.inflate(R.layout.fragment_danh_muc_san_pham, container, false);
+        return view;
     }
 
 }
